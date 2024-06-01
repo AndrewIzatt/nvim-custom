16736a10c8c74527b5531ff117f2de028c72aeca
workbuntu <izatt.andrew@gmail.com>
2024-05-31 19:19:20 -0600

    add clang lsp

--- init.lua
+++ init.lua
@@ -2,3 +2,9 @@ require("options")
 require("plugins")
 
 vim.g.node_host_prog = '~/.npm-global/bin/neovim-node-host'
+
+vim.lsp.start({
+    name = 'my-server-name',
+    cmd = {'name-of-language-server-executable'},
+    root_dir = vim.fs.root(0, {'pyproject.toml', 'setup.py'}),
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,15 +1,17 @@
 -- Sets up lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
+
 vim.opt.rtp:prepend(lazypath) -- prepend adds to the beginning of a list (or table in Lua) see :h rtp (runtimepath) for value of 'rtp'
 -- equal to vim.opt.rtp.prepend(vim.opt.rtp, lazypath)
 
@@ -24,52 +26,53 @@ require("lazy").setup({
         "nvim-treesitter/nvim-treesitter",
         config = function()
             require("nvim-treesitter.configs").setup({
-      -- A list of parser names, or "all" (parsers for c, lua, vim, vimdoc, and query should always be installed)
-              ensure_installed = { 
-                  "c", 
-                  "lua", 
-                  "vim", 
-                  "vimdoc", 
-                  "query", 
-                  "fish", 
-                  "python", 
-                  "css", 
-                  "bash", 
-                  "csv", 
-                  "asm", 
-                  "html",
-                  "javascript",
-                  "json",
-                  "sql"
+                -- A list of parser names, or "all" (parsers for c, lua, vim, vimdoc, and query should always be installed)
+                ensure_installed = { 
+                    "c", 
+                    "lua", 
+                    "vim", 
+                    "vimdoc", 
+                    "query", 
+                    "fish", 
+                    "python", 
+                    "css", 
+                    "bash", 
+                    "csv", 
+                    "asm", 
+                    "html",
+                    "javascript",
+                    "json",
+                    "sql"
                 },
 
-              -- Automatically install missing parsers when entering buffer
-              -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-              auto_install = true,
-            -- highlight module
-              highlight = {
-                enable = true,
-              },
-            -- incremental selection module
-              incremental_selection = {
-                  enable = true,
-                  keymaps = {
-                      init_selection = "<Leader>ss", -- set to `false` to disable one of the mappings
-                      node_incremental = "<Leader>si",
-                      scope_incremental = "<Leader>sc",
-                      node_decremental = "<Leader>sd",
-                  },
-              },
-
-              textobjects = {
+                -- Automatically install missing parsers when entering buffer
+                -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+                auto_install = true,
+                -- highlight module
+                highlight = {
+                    enable = true,
+                },
+                -- incremental selection module
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<Leader>ss", -- set to `false` to disable one of the mappings
+                        node_incremental = "<Leader>si",
+                        scope_incremental = "<Leader>sc",
+                        node_decremental = "<Leader>sd",
+                    },
+                },
+                -- Textobjects
+                textobjects = {
                     select = {
-                      enable = true,
+                        enable = true,
 
-              -- Automatically jump forward to textobj, similar to targets.vim
-                      lookahead = true,
+                        -- Automatically jump forward to textobj, similar to targets.vim
+                        lookahead = true,
 
-                      keymaps = {
-                        -- You can use the capture groups defined in textobjects.scm
+                        -- motions (which is why you don't use <Leader> key
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
                             ["af"] = "@function.outer",
                             ["if"] = "@function.inner",
                             ["ac"] = "@class.outer",
@@ -78,33 +81,33 @@ require("lazy").setup({
                             ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
                             -- You can also use captures from other query groups like `locals.scm`
                             ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-                      },
-                      -- You can choose the select mode (default is charwise 'v')
-                      --
-                      -- Can also be a function which gets passed a table with the keys
-                      -- * query_string: eg '@function.inner'
-                      -- * method: eg 'v' or 'o'
-                      -- and should return the mode ('v', 'V', or '<c-v>') or a table
-                      -- mapping query_strings to modes.
-                      selection_modes = {
+                        },
+                        -- You can choose the select mode (default is charwise 'v')
+                        --
+                        -- Can also be a function which gets passed a table with the keys
+                        -- * query_string: eg '@function.inner'
+                        -- * method: eg 'v' or 'o'
+                        -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                        -- mapping query_strings to modes.
+                        selection_modes = {
                             ['@parameter.outer'] = 'v', -- charwise
-                            ['@function.outer'] = 'V', -- linewise
+                            ['@function.outer'] = 'v', -- linewise
                             ['@class.outer'] = '<c-v>', -- blockwise
-                      },
-                      -- If you set this to `true` (default is `false`) then any textobject is
-                      -- extended to include preceding or succeeding whitespace. Succeeding
-                      -- whitespace has priority in order to act similarly to eg the built-in
-                      -- `ap`.
-                      --
-                      -- Can also be a function which gets passed a table with the keys
-                      -- * query_string: eg '@function.inner'
-                      -- * selection_mode: eg 'v'
-                      -- and should return true or false
-                      include_surrounding_whitespace = true,
+                        },
+                        -- If you set this to `true` (default is `false`) then any textobject is
+                        -- extended to include preceding or succeeding whitespace. Succeeding
+                        -- whitespace has priority in order to act similarly to eg the built-in
+                        -- `ap`.
+                        --
+                        -- Can also be a function which gets passed a table with the keys
+                        -- * query_string: eg '@function.inner'
+                        -- * selection_mode: eg 'v'
+                        -- and should return true or false
+                        include_surrounding_whitespace = true,
+                    },
                 },
-          },
-      })
-    end,
+            })
+        end,
     },
     {
         "nvim-treesitter/nvim-treesitter-textobjects",
