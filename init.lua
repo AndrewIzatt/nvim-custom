c94708b77b7c201d774a76ecebee4cc4906d9db7
AndrewIzatt <izatt.andrew@gmail.com>
2024-05-31 20:48:11 -0600

    add lspconfig and conditional logic for source of node.js depending on hostname

--- init.lua
+++ init.lua
@@ -1,10 +1,9 @@
 require("options")
 require("plugins")
 
-vim.g.node_host_prog = '~/.npm-global/bin/neovim-node-host'
-
-vim.lsp.start({
-    name = 'my-server-name',
-    cmd = {'name-of-language-server-executable'},
-    root_dir = vim.fs.root(0, {'pyproject.toml', 'setup.py'}),
-})
+-- Changes location of global node.js config depending on hostname
+if vim.fn.hostname() ~= "linuxmint" then
+    vim.g.node_host_prog = '~/.npm-global/bin/neovim-node-host'
+else
+    vim.g.node_host_prog = '/usr/local/bin/neovim-node-host'
+end
